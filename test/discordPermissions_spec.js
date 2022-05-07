7557af776f5c79ec38d3d462282b15c21a8feb40
javis86 <jcolombera@gmail.com>
2022-05-06 21:08:45 -0300

    discordPermissions tests added

--- test/discordPermissions_spec.js
+++ test/discordPermissions_spec.js
@@ -1,5 +1,5 @@
 const helper = require("node-red-node-test-helper");
-const discordMessageManager = require("../discord/discordMessageManager");
+const discordPermissions = require("../discord/discordPermissions");
 const discordToken = require("../discord/discord-token");
 const sinon = require("sinon");
 const discord = require('discord.js');
@@ -17,17 +17,17 @@ describe('Discord Message Manager Node', function () {
         getBotStub = sinon.stub(discordBotManager, 'getBot').resolves(stubDiscord);
     });
 
-    after(() => getBotStub.restore());    
+    after( () => getBotStub.restore() );
 
     afterEach(function () {
         helper.unload();
     });
 
     it('Node should be loaded', function (done) {
-        var flow = [{ id: "n1", type: "discordMessageManager", name: "test name", token: "24205d54014eb63f" }, 
+        var flow = [{ id: "n1", type: "discordPermissions", name: "test name", token: "24205d54014eb63f" }, 
                     { id: "24205d54014eb63f", type: "discord-token", name: "node boy"}];        
 
-        helper.load([discordToken, discordMessageManager], flow, () => {
+        helper.load([discordToken, discordPermissions], flow, () => {
             var n1 = helper.getNode("n1");
             try {
                 n1.should.have.property('name', 'test name');
@@ -39,22 +39,87 @@ describe('Discord Message Manager Node', function () {
     });
 
 
-    it('Error with no channel or user should fail', function (done) {
-        var flow = [{ id: "n1", type: "discordMessageManager", name: "test name", token: "24205d54014eb63f" },
+    it(`msg.user wasn't set correctly`, function (done) {
+        var flow = [{ id: "n1", type: "discordPermissions", name: "test name", token: "24205d54014eb63f" },
         { id: "24205d54014eb63f", type: "discord-token", name: "node boy" }];
 
-        helper.load([discordToken, discordMessageManager], flow, () => {
+        helper.load([discordToken, discordPermissions], flow, () => {
             let n1 = helper.getNode("n1");
             const nodeRedMsg = { payload: {}, _msgid: 'dd3be2d56799887c' };
 
             n1.receive(nodeRedMsg);
             n1.on('call:error', call => {
-                call.should.be.calledWithExactly('to send messages either msg.channel or msg.user needs to be set', nodeRedMsg);
+                call.should.be.calledWithExactly(`msg.user wasn't set correctly`, nodeRedMsg);
                 done();
             });
         });
     });
 
+    it(`msg.guild wasn't set correctly`, function (done) {
+        var flow = [{ id: "n1", type: "discordPermissions", name: "test name", token: "24205d54014eb63f" },
+        { id: "24205d54014eb63f", type: "discord-token", name: "node boy" }];
+
+        helper.load([discordToken, discordPermissions], flow, () => {
+            let n1 = helper.getNode("n1");
+            const nodeRedMsg = { payload: {}, _msgid: 'dd3be2d56799887c', user: "99991231931" };
+
+            n1.receive(nodeRedMsg);
+            n1.on('call:error', call => {
+                call.should.be.calledWithExactly(`msg.guild wasn't set correctly`, nodeRedMsg);
+                done();
+            });
+        });
+    });
+
+    it('Get Roles keep input _msgid and input properties on the output', function (done) {
+        stubDiscord.guilds = sinon.createStubInstance(discord.GuildManager);
+        let stubGuild = sinon.createStubInstance(discord.Guild);
+        stubGuild.members = sinon.createStubInstance(discord.GuildMemberManager);
+        const user = {
+            name: "Juancito",
+            roles: {
+                cache: {
+                    each: (func) => {
+                        func("role1");
+                        func("role2");
+                        func("role3");                        
+                    }
+                }
+            }
+        };
+        stubGuild.members.fetch.resolves( user );
+        stubDiscord.guilds.fetch.resolves( stubGuild );
+
+        let flow = [{ id: "n1", type: "discordPermissions", name: "test name", token: "24205d54014eb63f", wires: [["n2"]] },
+        { id: "24205d54014eb63f", type: "discord-token", name: "node boy" },
+        { id: "n2", type: "helper" }];
+
+        helper.load([discordToken, discordPermissions], flow, () => {
+            let n1 = helper.getNode("n1");
+            let n2 = helper.getNode("n2");
+            let nodeRedMsg = { _msgid: 'dd3be2d56799887c', user: "99991231931", guild: "111111111", payload: "Hello there", channel: "1111111111", topic: "10" };
+
+            n1.receive(nodeRedMsg);
+            n1.on('call:error', call => {
+                done(call);
+            });
+            n2.on("input", function (msg) {
+                try {
+                    msg.should.have.property('payload', ["role1", "role2", "role3"]);
+                    msg.should.have.property('_msgid', nodeRedMsg._msgid);
+                    msg.should.have.property('channel', nodeRedMsg.channel);
+                    msg.should.have.property('topic', nodeRedMsg.topic);
+                    msg.should.have.property('user', nodeRedMsg.user);
+                    msg.should.have.property('guild', nodeRedMsg.guild);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            });
+        });
+    });
+
+    /*
     it('Action type incorrect value should fail', function (done) {
         var flow = [{ id: "n1", type: "discordMessageManager", name: "test name", token: "24205d54014eb63f" },
         { id: "24205d54014eb63f", type: "discord-token", name: "node boy" }];
@@ -402,4 +467,5 @@ describe('Discord Message Manager Node', function () {
             });
         });
     });
+    */
 });
