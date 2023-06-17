3dabae079abd6654891e16ae8dd7d974a2f7e7ad
javis86 <jcolombera@gmail.com>
2023-06-17 13:16:35 -0300

    discordChannelName now with try/catch to prevent nodered crashes

--- discord/discordChannelName.js
+++ discord/discordChannelName.js
@@ -1,21 +1,36 @@
-module.exports = function(RED) {
-    var discordBotManager = require('./lib/discordBotManager.js');
-    function discordChannelName(config) {
-        RED.nodes.createNode(this, config);
-        var configNode = RED.nodes.getNode(config.token);
-        var node = this;
+module.exports = function (RED) {
+  var discordBotManager = require('./lib/discordBotManager.js');
+  function discordChannelName(config) {
+    RED.nodes.createNode(this, config);
+    var configNode = RED.nodes.getNode(config.token);
+    var node = this;
 
-   discordBotManager.getBot(configNode).then(function (bot) {
-      node.on('input', function(msg) {
-    const channel = config.channel || msg.channel || null;
-    bot.channels.cache.get(channel).setName(msg.name);
+    discordBotManager.getBot(configNode).then(function (bot) {
+      node.on('input', function (msg) {        
+        try {
+          const channel = config.channel || msg.channel || null;
+          bot.channels.cache.get(channel).setName(msg.name);
+
+          node.status({
+            fill: "green",
+            shape: "dot",
+            text: `Channel name was changed`
+          });
+          
+        } catch (error) {
+          node.status({
+            fill: "red",
+            shape: "dot",
+            text: error
+          });
+        }
       });
 
-    node.on('close', function() {
-                discordBotManager.closeBot(bot);
-            }); 
+      node.on('close', function () {
+        discordBotManager.closeBot(bot);
+      });
 
-});
-};
+    });
+  };
   RED.nodes.registerType("discordChannelName", discordChannelName);
 };
