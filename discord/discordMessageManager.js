f24c916d68254d015f3a8d7864064cff6c9a8e40
Julia Soria-Smith <juliasoriasmith@gmail.com>
2023-07-09 09:28:43 -0400

    Added info message function

--- discord/discordMessageManager.js
+++ discord/discordMessageManager.js
@@ -159,6 +159,15 @@ module.exports = function (RED) {
           }
         }
 
+        const infoMessage = async () => {
+          try {
+            let message = await getMessage(channel, messageId)
+            setSuccess(`message ${message.id} info obtained`, message);
+          } catch (err) {
+            setError(err);
+          }
+        }
+
         const deleteMessage = async () => {
           try {
             let message = await getMessage(channel, messageId);
@@ -310,6 +319,9 @@ module.exports = function (RED) {
             break;
           case 'crosspost':
             await crosspostMessage();
+            break;
+          case 'info':
+            await infoMessage();
             break;
           default:
             setError(`msg.action has an incorrect value`)
